7aa21247d00c2c21c9ab49a95e458d7d13cd3866
Long <trangiahoanglong@yahoo.com>
2022-02-08 20:49:08 +0700

    10.10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import DATA from './data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h5>Some data:</h5>
+      <ul>
+        { 
+          DATA.map((element, index)=>            
+            <li key={index}>{element.Name}</li>)
+        }
+      </ul>
     </div>
   );
 }
