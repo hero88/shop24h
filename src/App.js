6cda7d412f8c780044c9e34fe63e186d488a3272
Long <trangiahoanglong@yahoo.com>
2022-02-18 23:17:31 +0700

    16.30

--- src/App.js
+++ src/App.js
@@ -9,17 +9,32 @@ import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ContentComponent/ProductList';
 
 import ProductItems from './data.json';
+import Login from './components/ContentComponent/Login';
+
+import {useState, useEffect} from 'react';
+import {auth} from './firebase'
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    console.log("App user: ", user);
+    auth.onAuthStateChanged((result) => {
+      setUser(result);
+    })
+  },[user]);  
+
   return (
     <div >
-      <HeaderComponent/>      
+      <HeaderComponent currentUser={user}/>      
       <Row className='mt-5 p-4'>
           <Breadcrumb tag="nav">
             <BreadcrumbItem tag="a" href="/">Home</BreadcrumbItem>            
           </Breadcrumb>
       </Row>     
+      <br/>
       <Routes>
+        <Route path='/login' element={<Login sendUser={setUser}/>}/>
         <Route path='/' element={<HomepageContent/>}/>
         <Route path='product' element={<ProductList data={ProductItems}/>}/>
       </Routes> 
