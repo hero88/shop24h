d7aab37f04b5e63eb5dbaac494c7edfe96e3460b
Long <trangiahoanglong@yahoo.com>
2022-02-23 20:09:16 +0700

    update

--- src/components/ContentComponent/Login.js
+++ src/components/ContentComponent/Login.js
@@ -2,23 +2,41 @@ import { TextField, Button, Grid, Box } from '@mui/material';
 import {auth, googleProvider} from './../../firebase';
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Login({sendUser}) {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const changePasswordHandler = e => setPassword(e.target.value);
+    const changeEmailHandler = e => setEmail(e.target.value);
 
     const loginGoogle = () => {
         auth.signInWithPopup(googleProvider)
         .then((result) => {
-            console.log(result);
+            toast.success("Đăng nhập thành công!");
             setUser(result.user);            
             navigate('/');
             sendUser(user);
         })
-        .catch((error) => {
-            console.log(error)
+        .catch(() => {
+            toast.error("Email/Password sai rồi!");
         })
     }    
+
+    const loginEmail = () => {
+        auth.signInWithEmailAndPassword(email, password)
+        .then((result)=> {
+            toast.success("Đăng nhập thành công!");
+            setUser(result.user);
+            navigate('/');
+            sendUser(user);
+        })
+        .catch(() => toast.error("Email/Password sai rồi!"))
+    }
 
     return(
         <Box
@@ -44,16 +62,16 @@ function Login({sendUser}) {
                     <p>-----OR-----</p>
                 </Grid>
                 <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
-                    <TextField placeholder='Username' variant='outlined' label="Username"/>
+                    <TextField placeholder='Username' variant='outlined' label="Username" onChange={changeEmailHandler}/>
                 </Grid>
                 <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
-                    <TextField placeholder='Password' variant='outlined' label="Password"/>
+                    <TextField placeholder='Password' variant='outlined' label="Password" onChange={changePasswordHandler} type='password'/>
                 </Grid>
                 <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
-                    <Button variant='contained' color='success' style={{borderRadius: 10, margin: 20}}>Sign in</Button>
+                    <Button variant='contained' color='success' style={{borderRadius: 10, margin: 20}} onClick={loginEmail}>Sign in</Button>
                 </Grid>
             </Grid>
-            
+            <ToastContainer autoClose={2000}/>
         </Box>
     )
 }
