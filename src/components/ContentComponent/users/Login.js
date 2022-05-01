f10c0c5ed79a1ed56e3cc6bbd9daa3cdc50458cc
Long <trangiahoanglong@yahoo.com>
2022-05-01 13:21:50 +0700

    update google captcha

--- src/components/ContentComponent/users/Login.js
+++ src/components/ContentComponent/users/Login.js
@@ -1,14 +1,21 @@
-import { TextField, Button, Grid, Box } from '@mui/material';
+import { TextField, Button, Grid, Box} from '@mui/material';
 import {auth, googleProvider} from '../../../firebase';
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import ResetPasswordModal from '../../modal/ResetPasswordModal';
 import { fetchApi } from '../../../api';
 
+import {useGoogleReCaptcha} from 'react-google-recaptcha-v3';
+
 function Login({sendUser}) {
     const baseURL = "https://vast-castle-13621.herokuapp.com/customers/";
-    const navigate = useNavigate();  
+    const captchaURL = "https://vast-castle-13621.herokuapp.com/checkcaptcha";
+    const navigate = useNavigate();
+    
+    const { executeRecaptcha } = useGoogleReCaptcha();
+
+    const [showLogin, setShowLogin] = useState(false);
 
     const [user, setUser] = useState(null);  
     const [email, setEmail] = useState("");
@@ -46,7 +53,9 @@ function Login({sendUser}) {
             return false;
         })
     }
+
     const loginGoogle = () => {
+        handleReCaptchaVerify();
         auth.signInWithPopup(googleProvider)
         .then((result) => {
             toast.success("Đăng nhập thành công!");
@@ -71,6 +80,7 @@ function Login({sendUser}) {
     }    
 
     const loginEmail = () => {
+        handleReCaptchaVerify();
         auth.signInWithEmailAndPassword(email, password)
         .then((result)=> {
             toast.success("Đăng nhập thành công!");
@@ -86,44 +96,85 @@ function Login({sendUser}) {
         if (e.key === 'Enter') loginEmail();
     }
 
+    // Create an event handler so you can call the verification on button click event or form submit
+    const handleReCaptchaVerify = useCallback(async () => {
+        if (!executeRecaptcha) {
+            console.log('Execute recaptcha not yet available');
+            return;
+        }
+
+        const token = await executeRecaptcha('submit');
+        // Do whatever you want with the token
+        let reqOptions = {
+            body: JSON.stringify({token: token}),
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        }
+        fetchApi(captchaURL, reqOptions)
+        .then(res=>{
+            if (res.message === 'Successful')
+                setShowLogin(true);
+            else {
+                toast.error("Failed to verify human!"); 
+                setShowLogin(false);
+                return;
+            }
+        })
+        .catch(err => console.log(err))
+    },[executeRecaptcha, setShowLogin]);
+
     return(
         <Box
             component="form"
             sx={{
                 '& .MuiTextField-root': { m: 1, width: '25ch' },
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
             }}
             noValidate
             autoComplete="off"        
-            mt={5}
-        >
-            <Grid container spacing={1}>
-                <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
-                    <Button 
-                        variant='contained' 
-                        color='error' 
-                        style={{borderRadius: 10, margin: 20}}
-                        onClick={loginGoogle}
-                    >
-                        Sign in with Google                    
-                    </Button>
+            mt={3}
+        >  
+            {
+                showLogin ? 
+                <Grid container spacing={1}>
+                    <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
+                        <Button 
+                            variant='contained' 
+                            color='error' 
+                            style={{borderRadius: 10, margin: 20}}
+                            onClick={loginGoogle}
+                        >
+                            Sign in with Google                    
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
+                        <p>-----OR-----</p>
+                    </Grid>
+                    <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
+                        <TextField label="Username" onChange={changeEmailHandler} />
+                    </Grid>
+                    <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
+                        <TextField label="Password" onChange={changePasswordHandler} type='password' onKeyPress={handleKeyPress}/>
+                    </Grid>
+                    <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
+                        
+                    </Grid>
+                    <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
+                        <Button variant='contained' color='success' style={{borderRadius: 10, margin: 20}} onClick={loginEmail}>Sign in</Button>
+                    </Grid>
+                    <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
+                        <Button onClick={()=>setResetPasswordModal(true)}>Quên mật khẩu?</Button>
+                        <Button onClick={()=> navigate('/signup')}>Tạo tài khoản mới!</Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
-                    <p>-----OR-----</p>
-                </Grid>
-                <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
-                    <TextField label="Username" onChange={changeEmailHandler} />
-                </Grid>
-                <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>                    
-                    <TextField label="Password" onChange={changePasswordHandler} type='password' onKeyPress={handleKeyPress}/>
-                </Grid>
-                <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
-                    <Button variant='contained' color='success' style={{borderRadius: 10, margin: 20}} onClick={loginEmail}>Sign in</Button>
-                </Grid>
-                <Grid item xs={12} md={12} sm={12} lg={12} style={{textAlign:'center'}}>
-                    <Button onClick={()=>setResetPasswordModal(true)}>Quên mật khẩu?</Button>
-                    <Button onClick={()=> navigate('/signup')}>Tạo tài khoản mới!</Button>
-                </Grid>
-            </Grid>
+                : <Button onClick={handleReCaptchaVerify} variant='contained'>Verify human</Button>
+            }
+            
             <ResetPasswordModal resetModal={resetPasswordModal} setReset={setResetPasswordModal}/>
         </Box>
     )
